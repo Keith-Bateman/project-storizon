adcc0fec1bd6445a3d8c945d0cc51fbe8bca08fb
Keith Bateman <kbateman@hawk.iit.edu>
2018-04-24 11:06:10 -0500

    Added licensing information

--- src/ui/Edit.java
+++ src/ui/Edit.java
@@ -1,3 +1,29 @@
+/*
+Storizon
+A free inventory management application
+
+Copyright (C) 2018 Keith Bateman, Michael Kotlyar
+
+Email: 
+kbateman@hawk.iit.edu
+mkotlyar@hawk.iit.edu
+
+This file is part of Storizon
+
+Storizon is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version
+
+Storizon is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Storizon. If not, see http://www.gnu.org/licenses/.
+*/
+
 package ui;
 
 import org.gnome.gtk.*;
